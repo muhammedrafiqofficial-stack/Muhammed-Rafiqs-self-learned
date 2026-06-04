d580f55184a8329cf32e771cac078207cd7f9eac
muhammedrafiqofficial-stack <muhammedrafiqofficial@gmail.com>
2026-06-04 16:40:45 +0500

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,31 @@
+#Task 1
+Name = "Muhammed Rafiq"
+Age = 16
+City = "Karachi"
+isStudent = True
+print(Name, Age, City, isStudent)
+#Task 2
+name = "ali"
+age = 18
+Isstudent = True
+print(type(name))
+print(type(age))
+print(type(Isstudent))
+#Task 3
+marks = "95"
+print(int(marks))
+#Task 4
+phoneNumber = 123456789
+print(str(phoneNumber))
+#Task 5
+userName = "Ahmed"
+print(bool(userName))
+print(type(userName))
+#Task 6
+Name1 = "Muhammed Rafiq"
+Age1 = 16
+favcolour = "Black"
+student = True
+print(Name1, Age1, favcolour, student)
+
+
