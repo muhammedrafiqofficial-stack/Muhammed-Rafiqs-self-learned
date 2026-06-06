272a687684680293e3bd12a8171f6e624133afef
muhammedrafiqofficial-stack <muhammedrafiqofficial@gmail.com>
2026-06-06 22:02:19 +0500

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,29 @@
+#Task 1
+name = "My name is"
+python = "I am learning PythoPython is fun!"
+print(name + "Muhammed Rafiq")
+print(python)
+#Task 2
+name2 = "Muhammed Rafiq"
+age = 12
+city = "Karachi"
+print(name2)
+print(age)
+print(city)
+#Task 3
+num1 = 15
+num2 = 10
+print(num1 + num2)
+#Task 4
+a = 8
+b = 5
+print(a*b)
+#Task 5
+print(20 + 10)
+print(20 - 10)
+print(20 * 10)
+print(20 / 10)
+print(20 % 3)
+print(20 // 3)
+print(2 ** 4)
+
